4e3ea6beb6fc024a4ddecc120fb497b519ec8a56
paradox <136229133+kavindachamith@users.noreply.github.com>
2025-02-18 18:50:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~yYEHEaII#MiY0rlPFuiDr-moJxDDY3sXJJ4MBHeAqULcGYtPyruk",
 // For extra thing : BHASHI-MD~(megafileid)
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://kavindachamith13:20060705@cluster0.gcank.mongodb.net",
 
     
  
